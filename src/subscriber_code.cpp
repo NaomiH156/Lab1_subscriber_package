e84319921f46abf6481487041dfc27c3549cd5ef
Naomi Hourihane <jdh156@case.edu>
2019-09-10 16:24:29 -0400

    Naomi Hourihane Lab 1 subscriber package FINAL

--- src/subscriber_code.cpp
+++ src/subscriber_code.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "publisher_package/topic.h"
 
 void chatterCallback ( const std_msgs :: String :: ConstPtr & msg )
 {
@@ -9,7 +10,7 @@ int main ( int argc , char ** argv )
 {
 ros :: init ( argc , argv , "subscriber_node" );
 ros :: NodeHandle n ;
-ros :: Subscriber sub = n . subscribe ( "publisher_topic" , 1000 , chatterCallback );
+ros :: Subscriber sub = n . subscribe ( "topic" , 1000 , chatterCallback );
 ros :: spin ();
 return 0 ;
 }
